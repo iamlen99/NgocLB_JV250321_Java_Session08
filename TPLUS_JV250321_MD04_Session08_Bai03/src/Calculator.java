eddfbc71b38d26407ddc802b129f3cbafceb64fd
iamlen99 <allblue2903@gmail.com>
2025-07-02 18:34:30 +0700

    Upload bài tập MD4 session08_03

---
+++ TPLUS_JV250321_MD04_Session08_Bai03/src/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public int add(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public int multiply(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+}
